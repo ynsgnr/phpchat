a0704e866c7cd39795ca3bf22ac6617074ac495e
Yunus Güngör <yunusgnr@gmail.com>
2019-08-01 02:35:12 +0200

    more user functions yayy

--- api/user/User.php
+++ api/user/User.php
@@ -5,13 +5,31 @@ class User{
     private $session_id;
     private $connection;
 
-    function __construct(PDO $connection) {
+    function __construct(PDO $connection, $username) {
         $this->connection = $connection;
+        $this->setUserId($username);
     }
 
-    public function create_session(){
+    private function setUserId($username){
+        $query = $this->connection->prepare('SELECT (id) FROM users WHERE username=(?)');
+        $query->execute([$username]);
+        $this->user_id=$query->fetchAll()[0]['id'];
+    }
+
+    public function getUserId(){
+        return $this->user_id;
+    }
+
+    public function createSession(){
         //Create a new session on sessions table
         //Then return the unique id
+        //This function can be altered to add username password login later on
+        if(isset($this->user_id) === false){
+
+        } 
+        $query = $this->connection->prepare('INSERT INTO sessions (userid) VALUES (?)');
+        $query->execute([$this->user_id]);
+
     }
 
 
--- tests/unit/api/user/User.php
+++ tests/unit/api/user/User.php
@@ -12,12 +12,35 @@ class User_Test extends TestCase
     {
         $this->expectException(ArgumentCountError::class);
         $this->user = new User();
+        $db = new Database();
+        $this->expectException(ArgumentCountError::class);
+        $this->user = new User($db->connect2database());
     }
-
+    
     public function testConstruct()
     {
         $db = new Database();
-        $this->user = new User($db->connect2database());
+        $this->user = new User($db->connect2database(),"testuser");
         $this->assertInstanceOf(User::class, $this->user);
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testUserid()
+    {
+        $db = new Database();
+        $this->user = new User($db->connect2database(),"testuser");
+        $this->assertEquals(1,$this->user->getUserId());
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testCrateSession()
+    {
+        $db = new Database();
+        $this->user = new User($db->connect2database(),"testuser");
+        $this->assertInstanceOf("int", $this->user->createSession());
     }
 }
