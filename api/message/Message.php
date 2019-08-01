dc5295df4c6a39fca3c3e5827c224bf26a6c9f47
Yunus Güngör <yunusgnr@gmail.com>
2019-08-01 07:21:14 +0200

    More message functions yayy

--- api/message/Message.php
+++ api/message/Message.php
@@ -1,26 +1,43 @@
 <?php
+require_once('api/user/User.php');
+
 class Message{
 
     private $message_id;
-    private $connection;
 
     public $sender;
     public $reciever;
     public $context;
+    public $sendat;
 
-    function __construct(PDO $connection) {
-        $this->connection = $connection;
-    }
-
-    public function send(){
+    public function send(PDO $connection){
         //Add to db
-        if(isset($this->sender) === false and isset($this->reciever) === false and isset($this->context) === false){
+        if(isset($connection) === false or isset($this->sender) === false or isset($this->reciever) === false or isset($this->context) === false){
             throw new InvalidArgumentException("Please set sender,reciever and context!", 1);
         }
-        $query = $this->connection->prepare('INSERT INTO messages (context,sender,reciever,sendat) VALUES (?,?,?,NOW())');
+        $query = $connection->prepare('INSERT INTO messages (context,sender,reciever,sendat) VALUES (?,?,?,NOW())');
         $query->execute([$this->context,$this->sender,$this->reciever]);
-        $this->message_id=$this->connection->lastInsertId();
+        $this->message_id=$connection->lastInsertId();
         return $this->message_id;
+    }
+
+    public function receiveAllMessages(PDO $connection,User $user){
+        if(is_null($user->getUserId()) === true ){
+            throw new InvalidArgumentException("Please set userid of user", 1);
+        }
+        $messages = array();
+        $query = $connection->prepare('SELECT context,sender,sendat FROM messages WHERE reciever==(?) ORDER BY sendat DESC');
+        $query->execute([$user->getUserId()]);
+        $fetched = $query->fetchAll();
+        foreach ($fetched as $m) {
+            $message = new Message();
+            $message->sender = $m['sender'];
+            $message->reciever = $m['reciever'];
+            $message->context = $m['context'];
+            $message->sendat = $m['sendat'];
+            array_push($messages,$message); 
+        }
+        return $messages;
     }
 }
 ?>
--- tests/unit/api/message/Message.php
+++ tests/unit/api/message/Message.php
@@ -8,16 +8,9 @@ class Message_Test extends TestCase
 {
     private $message;
 
-    public function testConstructEmpty()
-    {
-        $this->expectException(ArgumentCountError::class);
-        $this->user = new Message();
-    }
-    
     public function testConstruct()
     {
-        $db = new Database();
-        $this->message = new Message($db->connect2database());
+        $this->message = new Message();
         $this->assertInstanceOf(Message::class, $this->message);
     }
 
@@ -27,9 +20,9 @@ class Message_Test extends TestCase
     public function testSend()
     {
         $db = new Database();
-        $this->message = new Message($db->connect2database());
+        $this->message = new Message();
         $this->expectException(InvalidArgumentException::class);
-        $this->message->send();
+        $this->message->send($db->connect2database());
     }
 
     /**
@@ -38,9 +31,36 @@ class Message_Test extends TestCase
     public function testSendWithValues()
     {
         $db = new Database();
-        $this->message = new Message($db->connect2database());
+        $this->message = new Message();
         $this->message->sender = 1;
         $this->message->reciever = 7;
-        $this->assertIsNumeric($this->message->send());
+        $this->expectException(InvalidArgumentException::class);
+        $this->message->send($db->connect2database());
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testSendWithFullValues()
+    {
+        $db = new Database();
+        $this->message = new Message();
+        $this->message->sender = 1;
+        $this->message->reciever = 7;
+        $this->message->context = "Message";
+        $this->assertIsNumeric($this->message->send($db->connect2database()));
+    }
+    
+    /**
+     * @depends testConstruct
+     */
+    public function testRecieve()
+    {
+        $db = new Database();
+        $connection = $db->connect2database();
+        $this->message = new Message();
+        $user = new User($connection);
+        $user->createSession("testuser");
+        $this->assertIsArray($this->message->receiveAllMessages($connection,$user));
     }
 }
